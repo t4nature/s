80378ae943db08a72ce54c573eaf507f200a9db9
Ryder Freeman <t4nature@proton.me>
2024-11-02 23:22:33 -0700

    edit

--- .gnupg/trezor/run-agent.sh
+++ .gnupg/trezor/run-agent.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export PATH="/home/rt/.config/py3/bin:/home/rt/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/usr/local/go/bin:/usr/local/jdk1.8.0_211/bin:/usr/local/clang/bin:/usr/local/jdk1.8.0_211/jre/bin:/home/rt/go/bin:/www/server/mysql/bin:/home/rt/.cargo/bin:/home/rt/.local/bin:/usr/local/go/bin:/usr/local/jdk1.8.0_211/bin:/usr/local/clang/bin:/usr/local/jdk1.8.0_211/jre/bin:/home/rt/go/bin:/www/server/mysql/bin:/home/rt/.cargo/bin:/home/rt/.local/bin"
-"/home/rt/.config/py3/bin/trezor-gpg-agent" -vv $*
+export PATH="/usr/local/py3/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:::::/home/rd/go/bin::/home/rd/.cargo/bin:/home/rd/.local/bin:::::/home/rd/go/bin::/home/rd/.cargo/bin:/home/rd/.local/bin"
+"/usr/local/py3/bin/trezor-gpg-agent" -vv $*
